fae08206ae60c03eeea6cf7edbcbde66902d45a5
SOUMITARAY <SOUMITARAY@DESKTOP-9D4C8ON>
2019-01-22 09:16:31 +0530

    Week 2 tc _V3

--- Selenium-Elearning-Automation-FrameWork/src/com/training/pom/FilterMessag_Member.java
+++ Selenium-Elearning-Automation-FrameWork/src/com/training/pom/FilterMessag_Member.java
@@ -1,7 +1,13 @@
 package com.training.pom;
 
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+
 import java.util.List;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -9,14 +15,16 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
 
+
+
 public class FilterMessag_Member {
 	
 	WebDriver driver;
 	
-	public FilterMessag_Member() {
+	public FilterMessag_Member(WebDriver driver)
+	{
 		// TODO Auto-generated constructor stub
 		this.driver = driver; 
-
 		PageFactory.initElements(driver, this);
 	}
 
@@ -46,7 +54,11 @@ public class FilterMessag_Member {
 	@FindBy(xpath="(//table[@class='defaultTable'])[2]/tbody/tr[2]/td[3]")
 	WebElement subject;
 	
-	public void search_SentItems_Admin()
+	@FindBy(xpath = "(//*[@class='moveToTrash'])[2]")
+	WebElement delete_secondMsg;
+	
+	
+	public void search_Inbox_All()
 	{
 		this.menu_Personal.click();
 		this.submenu_Messages.click();
@@ -54,24 +66,80 @@ public class FilterMessag_Member {
 		Select select_type = new Select(msgbox_type);
 		select_type.selectByVisibleText("Inbox");
 		Select select_owner_type= new Select(msg_type);
-		select_owner_type.selectByVisibleText("Administration");
+		select_owner_type.selectByVisibleText("All");
 		this.submit_query.click();
 		
 		validate_dataset(); 
+		delete_message();
+		
 		
 	}
 	
 	public void validate_dataset()
 	{
-		
-		List<WebElement> dataset = driver.findElements(By.xpath("(//table[@class='defaultTable'])[2]/tbody/tr[2]"));
+		System.out.println("in validate method");
+		List<WebElement> dataset = driver.findElements(By.xpath("(//table[@class='defaultTable'])[2]/tbody/tr[2]/td"));
 		int dataset_cols = dataset.size();
+		System.out.println(dataset_cols);
+		String []recordset = new String[dataset_cols]; 
 		
-		for (int i=1;i<=dataset_cols;i++)
+		for (int i=2;i<=dataset_cols;i++)
 		{
-			List<WebElement> dataset_col_data = driver.findElements(By.xpath("(//table[@class='defaultTable'])[2]/tbody/tr[2]"));
+			WebElement dataset_col_data = driver.findElement(By.xpath("(//table[@class='defaultTable'])[2]/tbody/tr[2]/td["+i+"]"));
+			System.out.println(i+" " + dataset_col_data);
+			recordset[i-2] = dataset_col_data.getText();
+						
+			switch(i)
+			{
+			case 1:
+				if(dataset_col_data.isSelected())
+				{continue;}
+				else
+				{
+					dataset_col_data.click();
+				}
+				break;	
+				
+			//assertFalse((WebElement) dataset_col_data).isSelected());
+			
+			case 2:				
+				
+				boolean check_col2 = (dataset_col_data.getText().equalsIgnoreCase("20/01/2019"));
+				System.out.println(check_col2);
+				break;
+										
+			case 3:
+			
+				boolean check_col3 = (dataset_col_data.getText().equalsIgnoreCase("Transaction feedback"));
+				System.out.println(check_col3);
+				break;
+			
+			case 4:
+			//	assertEquals(dataset_col_data.getText(), "System");
+				
+				boolean check_col4 = dataset_col_data.getText().equalsIgnoreCase("System");
+				System.out.println(check_col4);
+				break;
+				
+			default:
+				break;
+			}	
+				
 		}
 		
+	}
+	
+	public void delete_message()
+	{
+		//delete a message from Inbox and check in trash if row exist
+		this.delete_secondMsg.click();
+		Alert alert = driver.switchTo().alert();
+		alert.getText();  
+		assertTrue(alert.getText().contains("The messages were moved to trash"));
+		alert.accept();
+		Select select_type = new Select(msgbox_type);
+		select_type.selectByVisibleText("Trash");
+		assertTrue(datatable != null); 
 		
 	}
 }
---
+++ Selenium-Elearning-Automation-FrameWork/tests/com/training/sanity/tests/FilterMessage_MemberTest.java
@@ -0,0 +1,71 @@
+package com.training.sanity.tests;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.training.generics.ScreenShot;
+import com.training.pom.LoginPOM;
+import com.training.pom.FilterMessag_Member;
+import com.training.utility.DriverFactory;
+import com.training.utility.DriverNames;
+
+public class FilterMessage_MemberTest {
+	
+	private WebDriver driver;
+	private static String baseUrl;
+	private LoginPOM loginPOM;
+	private FilterMessag_Member filtermsg;
+	
+	private static Properties properties;
+	private static ScreenShot screenShot;
+	
+	
+	 @BeforeMethod
+	  public void beforeMethod() {
+	  }
+
+	  @AfterMethod
+	  public void afterMethod() {
+	  }
+
+	  @BeforeClass
+	  public void beforeClass() throws IOException {
+		  properties = new Properties();
+			FileInputStream inStream = new FileInputStream("./resources/others.properties");
+			properties.load(inStream);
+			baseUrl = properties.getProperty("baseURL");
+		   screenShot = new ScreenShot(driver); 
+		   driver = DriverFactory.getDriver(DriverNames.CHROME);
+		   loginPOM = new LoginPOM(driver); 
+		   filtermsg = new FilterMessag_Member(driver);
+			
+		   
+		// open the browser 
+		   driver.get(baseUrl);
+	  }
+	
+	  @AfterClass
+	  public void afterClass() {
+	  }
+
+	
+	
+  @Test
+  public void filtermsg_Inbox() {
+	  
+	  System.out.println("------Testcase : CYTC_048-------");
+	 	loginPOM.sendUserName("Soumita123");
+		loginPOM.sendPassword("test1234");
+		loginPOM.clickLoginBtn();
+		filtermsg.search_Inbox_All();
+		
+  }
+}
